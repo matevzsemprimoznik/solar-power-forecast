1d05646c268f0ef8d6ab4bee8533355bcd313ee2
Matevz <semprimoznik.matevz@gmail.com>
2024-06-09 14:38:04 +0200

    fix/model-management-api: model training

--- src/apps/model_management_api/utils/load_data.py
+++ src/apps/model_management_api/utils/load_data.py
@@ -5,20 +5,27 @@ from src.config.settings import settings
 
 
 def download_data_for_model_train():
-    os.environ["GIT_PYTHON_REFRESH"] = "quiet"
-    with dvc.api.open(
-        'data/processed/data_selected_features.csv',
-        repo=settings.DAGSHUB_REPO_URI,
-        mode='r',
-        remote_config={
-            "access_key_id": settings.DAGSHUB_ACCESS_KEY_ID,
-            "secret_access_key": settings.DAGSHUB_ACCESS_SECRET_KEY
-        }
-    ) as f:
-        if not os.path.exists('data/processed'):
-            os.makedirs('data/processed')
-        with open('data_selected_features.csv', 'w') as file:
-            file.write(f.read())
+    print("Downloading data for model training")
+
+    try:
+        os.environ["GIT_PYTHON_REFRESH"] = "quiet"
+        with dvc.api.open(
+                'data/processed/data_selected_features.csv',
+                repo=settings.DAGSHUB_REPO_URI,
+                mode='r',
+                remote_config={
+                    "access_key_id": settings.DAGSHUB_ACCESS_KEY_ID,
+                    "secret_access_key": settings.DAGSHUB_ACCESS_SECRET_KEY
+                }
+        ) as f:
+            print("Data downloaded successfully")
+            if not os.path.exists('data/processed'):
+                os.makedirs('data/processed')
+            with open('data/processed/data_selected_features.csv', 'w') as file:
+                file.write(f.read())
+
+    except Exception as e:
+        print("An error occurred while downloading data:", e)
 
 
 if __name__ == "__main__":
